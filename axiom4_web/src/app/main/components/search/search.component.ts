4184c416cd94a1e1b330398d3b08ffb51338cf16
Riccardo Giannetto <riccardo.giannetto@igt.com>
2023-08-17 21:27:49 +0200

    fix search shortcut

--- axiom4_web/src/app/main/components/search/search.component.ts
+++ axiom4_web/src/app/main/components/search/search.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { SearchModalComponent } from '../search-modal/search-modal.component';
 
 @Component({
@@ -7,11 +7,35 @@ import { SearchModalComponent } from '../search-modal/search-modal.component';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  modalRef: NgbModalRef | undefined;
+  debounceTime = 300;
+  opened = false;
+  @Output('click.single') singleClick = new EventEmitter();
+
+  ngOnInit() { }
+
+  @HostListener('window:keydown.meta.f', ['$event'])
+  public onKeyUp(eventData: KeyboardEvent) {
+    eventData.preventDefault();
+    eventData.stopImmediatePropagation();
+    if (!this.opened)
+      this.open();
+  }
   constructor(private _modalService: NgbModal) { }
 
   open() {
-    const modalRef = this._modalService.open(SearchModalComponent, { size: 'lg' });
-    modalRef.componentInstance.name = 'World';
+    this.modalRef = this._modalService.open(SearchModalComponent, { size: 'lg' });
+    this.modalRef.dismissed.subscribe(() => {
+      console.log("search dismissed")
+      this.opened = false;
+    })
+
+    this.modalRef.closed.subscribe(() => {
+      console.log("search closed")
+      this.opened = false;
+    })
+
+    this.opened = true
   }
 }
