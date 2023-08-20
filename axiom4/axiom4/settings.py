729ccb57b5083e4fea87d9fe9f22876236645e07
Riccardo Giannetto <riccardo.giannetto@igt.com>
2023-08-20 11:09:59 +0200

    upgrade requirements

--- axiom4/axiom4/settings.py
+++ axiom4/axiom4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "corsheaders",
+    'django.forms',
+    'markdownx',
     'Blog',
     'ImageUpload'
 ]
@@ -59,7 +62,7 @@ ROOT_URLCONF = 'axiom4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
