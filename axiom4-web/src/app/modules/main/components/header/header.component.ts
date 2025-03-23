818379bb4e381463d68f0fca5c827d08b468a824
Riccardo GIannetto <rgiannetto@gmail.com>
2025-03-23 16:02:10 +0100

    fix logo

--- axiom4-web/src/app/modules/main/components/header/header.component.ts
+++ axiom4-web/src/app/modules/main/components/header/header.component.ts
@@ -19,35 +19,16 @@ export class HeaderComponent implements OnInit {
   showMenu = false;
   showLogo = false;
   config: Configuration | undefined;
-  height = 0;
+  // height = 0;
 
   constructor(private configService: ConfigService) {
     this.landscape.addEventListener('change', (ev) => {
       console.log('landscape orientation', this.landscape.matches);
-      this.height = window.innerHeight;
-      console.log('height', this.height);
-
-      if (this.height > 375) {
-        this.showLogo = true;
-        this.showMenu = false;
-      } else {
-        this.showLogo = false;
-        this.showMenu = false;
-      }
     });
   }
 
   ngOnInit(): void {
     this.config = this.configService.getConfiguration();
-    this.height = window.innerHeight;
-
-    if (this.height > 375) {
-      this.showLogo = true;
-      this.showMenu = false;
-    } else {
-      this.showLogo = false;
-      this.showMenu = false;
-    }
   }
 
   landscape = window.matchMedia('(orientation: landscape)');
